b5af82d05e7ecbb67a8b2fa6dc7393f14987ac0e
bonig97 <bonig97@gmail.com>
2023-12-06 22:46:39 +0100

    Üse regex to find first and last digit in each text file line and sum up them

--- 1/read.go
+++ 1/read.go
@@ -1,25 +1,54 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
+	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strconv"
 )
 
 func main() {
 	f, err := os.Open("input.txt")
-
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer f.Close()
 
 	scanner := bufio.NewScanner(f)
+	re := regexp.MustCompile("[0-9]")
 
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		s := scanner.Text()
+		digits := re.FindAllString(s, -1)
+
+		if len(digits) >= 2 {
+			firstDigit, lastDigit := digits[0], digits[len(digits)-1]
+
+			firstInt, err := strconv.Atoi(firstDigit)
+			if err != nil {
+				log.Printf("Error: %v\n", err)
+				continue
+			}
+			lastInt, err := strconv.Atoi(lastDigit)
+			if err != nil {
+				log.Printf("Error: %v\n", err)
+				continue
+			}
+
+			sum := firstInt + lastInt
+			fmt.Printf("First: %d, Last: %d, Sum: %d\n", firstInt, lastInt, sum)
+		} else if len(digits) == 1 {
+			firstInt, err := strconv.Atoi(digits[0])
+			if err != nil {
+				log.Printf("Error: %v\n", err)
+				continue
+			}
+			fmt.Printf("First: %d, Sum: %d\n", firstInt, firstInt*2)
+		} else {
+			fmt.Println("No digits found")
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
