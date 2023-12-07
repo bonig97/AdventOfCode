9a02e356e4eb9585c4a76041894bc017a4752452
bonig97 <bonig97@gmail.com>
2023-12-07 21:45:53 +0100

    Add gitignore file

---
+++ 1/read.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(f)
+
+	scanner := bufio.NewScanner(f)
+	re := regexp.MustCompile("[0-9]")
+	totalSum := 0
+
+	for scanner.Scan() {
+		s := scanner.Text()
+		digits := re.FindAllString(s, -1)
+
+		if len(digits) >= 2 {
+			firstDigit, lastDigit := digits[0], digits[len(digits)-1]
+
+			firstInt, err := strconv.Atoi(firstDigit)
+			if err != nil {
+				log.Printf("Error: %v\n", err)
+				continue
+			}
+			lastInt, err := strconv.Atoi(lastDigit)
+			if err != nil {
+				log.Printf("Error: %v\n", err)
+				continue
+			}
+
+			sum := firstInt + lastInt
+			totalSum += sum
+		} else if len(digits) == 1 {
+			firstInt, err := strconv.Atoi(digits[0])
+			if err != nil {
+				log.Printf("Error: %v\n", err)
+				continue
+			}
+			totalSum += firstInt * 2
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Total sum: %d\n", totalSum)
+}
